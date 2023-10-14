434e7d8290907849036bcfd44b2632eb20fb8fc2
chandler ames <eddiehawk1991@gmail.com>
2023-10-14 01:55:06 -0700

    server.js update

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001; 
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public')); 
+
+app.use(express.static('public'));
+
+app.get('/api/example', (req, res) => {
+  res.json({ message: 'gguguyg' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
