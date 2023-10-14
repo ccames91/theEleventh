1b01d17c94239612e0e0413471cf967d7970c81a
chandler ames <eddiehawk1991@gmail.com>
2023-10-14 01:34:32 -0700

    routes post

--- Develop/public/routes/apiRoutes.js
+++ Develop/public/routes/apiRoutes.js
@@ -1,4 +1,8 @@
 app.get('/api/notes', (req, res) => {
     res.json(notes);
     });
-  
+    app.post('/api/notes', (req, res) => {
+        const newNote = req.body; 
+        notes.push(newNote);
+      });
+      
