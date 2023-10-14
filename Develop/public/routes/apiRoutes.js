ae63c2968662e18759a9c3a0d035e08500ac5d74
chandler ames <eddiehawk1991@gmail.com>
2023-10-14 01:35:22 -0700

    routes delete

--- Develop/public/routes/apiRoutes.js
+++ Develop/public/routes/apiRoutes.js
@@ -5,4 +5,8 @@ app.get('/api/notes', (req, res) => {
         const newNote = req.body; 
         notes.push(newNote);
       });
+      app.delete('/api/notes/:id', (req, res) => {
+        const noteId = req.params.id; 
+        notes = notes.filter(note => note.id != noteId);
+      });
       
